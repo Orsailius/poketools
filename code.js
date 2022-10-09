0a87c84e6f0e688b7f777af366eef406190a8f47
Orsailius <sumemail8472@gmail.com>
2022-10-09 13:26:40 -0400

    custom data 1

--- code.js
+++ code.js
@@ -9,16 +9,11 @@ var tabledata = [
 
 //create Tabulator on DOM element with id "example-table"
 var table = new Tabulator("#example-table", {
-    height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-    data:tabledata, //assign data to table
-    layout:"fitColumns", //fit columns to width of table (optional)
-    columns:[ //Define Table Columns
-        {title:"Name", field:"name", width:150},
-        {title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
-        {title:"Favourite Color", field:"col"},
-        {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
-    ],
+    height:405, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+    importFormat:"json",
 });
+
+table.setData(data);
 
 //trigger an alert message when the row is clicked
 table.on("rowClick", function(e, row){ 
