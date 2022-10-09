faaa2a64dcadf3e4523190e43f9cec2794f99dc8
Orsailius <sumemail8472@gmail.com>
2022-10-09 18:26:26 -0400

    stats

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 //define some sample data
 var tabledata = [
-    {id:1, name:"Bulbasaur", Types:"Grass,Poison", Speed:"45"},
+    {id:1, name:"Bulbasaur", Types:"Grass,Poison", HP:"45", Attack:"49",
+    Defense:"49", SpecialAttack:"65", SpecialDefense:"65", Speed:"45", },
 ];
 
 //create Tabulator on DOM element with id "example-table"
@@ -11,10 +12,30 @@ var table = new Tabulator("#example-table", {
  	columns:[ //Define Table Columns
 	 	{title:"Name", field:"name", width:150, frozen:true},
 	 	{title:"Types", field:"Types", hozAlign:"left"},
+         {title:"HP", field:"HP", formatter:"progress",
+         min:0,
+         max:255,
+         color:["red", "orange", "yellow", "green", "blue"],},
+         {title:"Attack", field:"Attack", formatter:"progress",
+         min:0,
+         max:255,
+         color:["red", "orange", "yellow", "green", "blue"],},
+         {title:"Defense", field:"Defense", formatter:"progress",
+         min:0,
+         max:255,
+         color:["red", "orange", "yellow", "green", "blue"],},
+         {title:"Special Attack", field:"Special Attack", formatter:"progress",
+         min:0,
+         max:255,
+         color:["red", "orange", "yellow", "green", "blue"],},
+         {title:"Special Defense", field:"Special Defense", formatter:"progress",
+         min:0,
+         max:255,
+         color:["red", "orange", "yellow", "green", "blue"],},
          {title:"Speed", field:"Speed", formatter:"progress",
          min:0,
          max:255,
-         color:["green", "orange", "red"],},
+         color:["red", "orange", "yellow", "green", "blue"],},
  	]
 });
 
